4ba32f3da5e439e40115000bc40fa912161f2141
grasssi <grassi.amine@hotmail.fr>
2023-05-21 22:40:42 +0200

    init d'app

---
+++ database/connect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+mongoose.connect('mongodb://localhost:27017/Badge_DataBase', options).then(connect => {
+    console.log("=> connect to databse successfully!")
+}).catch(err => {
+    console.log("=> connect to database with error :")
+    console.log(err);
+});
